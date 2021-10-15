6298da83637495b698230beaca741508df36eac4
ana2401 <77988773+ana2401@users.noreply.github.com>
2021-10-15 18:50:45 +0200

    Update main.c

--- Image/main.c
+++ Image/main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include "SDL2/SDL.h"
 
+void init_sdl()
+{
+    // Init only the video part.
+    // If it fails, die with an error message.
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+}
+
 /*
  * Function: loadImage
  * ----------------------------
@@ -20,6 +28,47 @@ SDL_Surface *loadImage(char *path)
     return image;
 }
 
+SDL_Surface* display_image(SDL_Surface *img)
+{
+    SDL_Surface *screen;
+
+    // Set the window to the same size as the image
+    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+    if (screen == NULL)
+    {
+        // error management
+        errx(1, "Couldn't set %dx%d video mode: %s\n",
+                img->w, img->h, SDL_GetError());
+    }
+
+    // Blit onto the screen surface
+    if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+        warnx("BlitSurface error: %s\n", SDL_GetError());
+
+    // Update the screen
+    SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+
+    // return the screen for further uses
+    return screen;
+}
+
+void wait_for_keypressed()
+{
+    SDL_Event event;
+
+    // Wait for a key to be down.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYDOWN);
+
+    // Wait for a key to be up.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYUP);
+}
+
 /*
  * Function: main
  * ----------------------------
@@ -30,8 +79,11 @@ SDL_Surface *loadImage(char *path)
  */
 int main(int argc, char **argv)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) == -1)
-        errx(1, "Coult not initialize SDL : %s\n", SDL_GetError());
+    init_sdl();
     SDL_Surface *image = loadImage("image.jpg");
-    // à finir
+    SDL_Surface *n_image = grayscale(image);
+    image = *blackWhite(n_image);
+    SDL_SaveBMP(image, "image2.jpg");
+    SDL_FreeSurface(image);
+    SDL_FreeSurface(n_image);
 }
