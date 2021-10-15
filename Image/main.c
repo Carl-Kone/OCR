04267b28ad5c6c57bac343134c264dc9a365c508
Bianca Onciul <bianca.onciul@epita.fr>
2021-10-15 14:40:41 +0200

    modified main

--- Image/main.c
+++ Image/main.c
@@ -1,23 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "SDL2/SDL.h"
+#include "remove_color.h"
 
-/*
- * Function: loadImage
- * ----------------------------
- *   loads an image
- *
- *    path: the path to the image
- *
- *   returns: a surface
- */
-SDL_Surface *loadImage(char *path)
+void init_sdl()
 {
-    SDL_Surface *image = NULL;
-    image = IMG_Load(path);
-    if (!image)
+    // Init only the video part.
+    // If it fails, die with an error message.
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+}
+
+SDL_Surface* load_image(char *path)
+{
+    SDL_Surface *img;
+
+    // Load an image using SDL_image with format detection.
+    // If it fails, die with an error message.
+    img = IMG_Load(path);
+    if (!img)
         errx(3, "can't load %s: %s", path, IMG_GetError());
-    return image;
+
+    return img;
+}
+
+SDL_Surface* display_image(SDL_Surface *img)
+{
+    SDL_Surface *screen;
+
+    // Set the window to the same size as the image
+    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+    if (screen == NULL)
+    {
+        // error management
+        errx(1, "Couldn't set %dx%d video mode: %s\n",
+                img->w, img->h, SDL_GetError());
+    }
+
+    // Blit onto the screen surface
+    if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+        warnx("BlitSurface error: %s\n", SDL_GetError());
+
+    // Update the screen
+    SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+
+    // return the screen for further uses
+    return screen;
+}
+
+void wait_for_keypressed()
+{
+    SDL_Event event;
+
+    // Wait for a key to be down.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYDOWN);
+
+    // Wait for a key to be up.
+    do
+    {
+        SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYUP);
 }
 
 /*
@@ -30,8 +75,11 @@ SDL_Surface *loadImage(char *path)
  */
 int main(int argc, char **argv)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) == -1)
-        errx(1, "Coult not initialize SDL : %s\n", SDL_GetError());
+    init_sdl();
     SDL_Surface *image = loadImage("image.jpg");
-    // à finir
+    SDL_Surface *n_image = grayscale(image);
+    image = *blackWhite(n_image);
+    SDL_SaveBMP(image, "image2.jpg");
+    SDL_FreeSurface(image);
+    SDL_FreeSurface(n_image);
 }
---
+++ Solver/grid_read.h
@@ -0,0 +1,8 @@
+// Header file of grid_read.c
+
+#ifndef grid_read_h
+#define grid_read_h
+
+const char* grid_read(FILE *fptr);
+
+#endif
