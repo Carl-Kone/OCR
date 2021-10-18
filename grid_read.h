15aa05a4690c78e4c38154dc447fe7a6c230b883
Carl Kone Takuefou <78765403+Carl-Kone@users.noreply.github.com>
2021-10-18 18:41:24 +0200

    Removed duplicated Solver files

--- grid_read.h
+++
@@ -1,8 +0,0 @@
-// Header file of grid_read.c
-
-#ifndef grid_read_h
-#define grid_read_h
-
-const char* grid_read(FILE *fptr);
-
-#endif
--- grid_write.h
+++
@@ -1,6 +0,0 @@
-#ifndef grid_write_h
-#define grid_write_h
-
-void grid_write(const char* str, const char *path);
-
-#endif
