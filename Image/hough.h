7227238ceb442b711f5687861c98ed718e709e85
ana2401 <77988773+ana2401@users.noreply.github.com>
2021-11-17 18:33:37 +0100

    Update hough.h

--- Image/hough.h
+++ Image/hough.h
@@ -6,7 +6,7 @@
 #include <math.h>
 
 double to_radian(int degree);
-int houghTransform(SDL_Surface* image, int** accumulator);
-int maxVal(int** accumulator, int rows, int cols)
+void houghTransform(SDL_Surface* image, int accumulator[][181]);
+void HoughMain(SDL_Surface* image);
 
 #endif
