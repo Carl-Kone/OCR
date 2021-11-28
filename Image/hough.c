357ef29189969876db34bdb91ed956c96bd71c02
ana2401 <77988773+ana2401@users.noreply.github.com>
2021-11-28 16:09:36 +0100

    Update hough.c

--- Image/hough.c
+++ Image/hough.c
@@ -24,6 +24,7 @@ void houghTransform(SDL_Surface* image, int accumulator[][181])
     double rho, rad;
     Uint32 pixel;
     Uint8 r, g, b;
+    int nb_r = 0;
     // iterate through the pixels of the image
     for(int y = 0; y < height; y++)
     {
@@ -33,18 +34,22 @@ void houghTransform(SDL_Surface* image, int accumulator[][181])
             pixel = get_pixel(image, x, y);
             SDL_GetRGB(pixel, image->format, &r, &g, &b);
             // enter if the pixel is white
-            if(r == 255)
+            if(r > 0)
             {
-                for(int theta = 0; theta < 181; ++theta)
-                {
+                nb_r++;
+                for(int theta = -90; theta < 91; theta++)
+               {
                     rad = to_radian(theta);
                     rho = x * cos(rad) + y * sin(rad);
                     // +1 in accumulator for each theta for each point
-                    accumulator[(int) rho + r_max][theta] += 1;
+                    accumulator[(int) rho + r_max][theta + 90] += 1;
+                    //if(accumulator[(int) rho + r_max][theta] > 10)
+                    // printf("accumulator[%d][%d] = %d\n", (int) rho + r_max, theta,  accumulator[(int) rho + r_max][theta]);
                 }
             }
         }
     }
+    //printf("nb_r = %d\n", nb_r);
 }
 
 /*
@@ -70,13 +75,15 @@ void coordinates(int accumulator[][181], int rows, int cols)
     {
         for(int x = 0; x < cols; x++)
         {
-            if(accumulator[y][x] != max)
+            if(accumulator[y][x] < max-300)
             {
-                // can I create a array of tuples for the coordinates? or am I supposed to create a structure?
                 accumulator[y][x] = 0;
             }
+            else if (accumulator[y][x] > 100)
+                printf("accumulator value = %d\n", accumulator[y][x]);
         }
     }
+    printf("coordinates done\n");
 }
 
 /*
@@ -95,13 +102,16 @@ void drawHough(SDL_Surface* image, int** accumulator, int rows, int cols)
 }
 */
 
-void HoughMain(SDL_Surface* image)
+void DrawLines(SDL_Surface* image)
 {
     int width = image->w;
+    printf("width = %d\n", width);
     int height = image->h;
+    printf("height = %d\n", height);
     int rho = (int) sqrt(width * width + height * height);
     int w = 181;
     int h = 2*rho+1;
+    printf("rho = %d\n", h);
     int accumulator[h][w];
     for(int j = 0; j < h; j++)
     {
@@ -111,19 +121,105 @@ void HoughMain(SDL_Surface* image)
         }
     }
     houghTransform(image, accumulator);
-    //coordinates(accumulator, 2*(rho+1), 181);
-    SDL_Surface *bimage = SDL_CreateRGBSurface(0, 180, 2*rho, 32, 0, 0, 0, 0);
+    coordinates(accumulator, h, w);
+    double x, y;
+    float a, b, origin, slope;
+    int x1, y1, x2, y2;
+    Uint32 pixel;
+    printf("before loop\n");
+    for(int j = 0; j < h; j++)
+    {
+        //printf("j == %d\n", j);
+        for(int i = 0; i < w; i++)
+        {
+            //printf("i == %d\n", i);
+            //printf("accumulator[%d][%d] = %d\n", j, i, accumulator[j][i]);
+            if(accumulator[j][i] > 0)
+            {
+                printf("accumulator[%d][%d] = %d\n", j, i, accumulator[j][i]);
+                a = cos(to_radian(i-90));
+                b = sin(to_radian(i-90));
+                x = a * j;
+                y = b * j;
+                x1 = (int)x + 1000*(-b);
+                y1 = (int)y + 1000*a;
+                //printf("x1 = %d, y1 = %d\n", x1, y1);
+                x2 = (int)x - 1000*(-b);
+                y2 = (int)y - 1000*a;
+                //printf("x2 = %d, y2 = %d\n", x2, y2);
+                //printf("y2 - y1 = %d\n", y2-y1);
+                //printf("x2 - x1 = %d\n", x2-x1);
+                if(x1 == x2)
+                {
+                    slope = 1;
+                    origin = 0;
+                }
+                else
+                {
+                    slope = (float)(y2-y1)/(x2-x1);
+                    origin = y2 - slope * x2;
+                }
+                //printf("origin = %f\n", origin);
+                //printf("origin should be = %f\n", y2 - slope * x2);
+                //printf("slope = %f\n", slope);
+                for(int n = 0; n < width; n++)
+                    {
+                        y2 = (int)slope * n + origin;
+                        printf("y2 = %d\n", y2);
+                        if(y2 >= 0 && y2 < height)
+                        {
+                            //printf("y2 = %d\n", y2);
+                            pixel = SDL_MapRGB(image->format, 255, 0, 0);
+                            //printf("x = %d, y = %d\n", n, y2);
+                            put_pixel(image, n, y2, pixel);
+                        }
+                    }
+            }
+        }
+    }
+}
+
+void HoughMain(SDL_Surface* image)
+{
+    int width = image->w;
+    printf("width = %d\n", width);
+    int height = image->h;
+    printf("height = %d\n", height);
+    int rho = (int) sqrt(width * width + height * height);
+    int w = 181;
+    int h = 2*rho+1;
+    printf("rho = %d\n", h);
+    int accumulator[h][w];
     for(int j = 0; j < h; j++)
     {
         for(int i = 0; i < w; i++)
         {
-            int val = accumulator[j][i];
-            Uint32 pixel = SDL_MapRGB(bimage->format, val, val, val);
+            accumulator[j][i] = 0;
+        }
+    }
+    houghTransform(image, accumulator);
+    DrawLines(image);
+    printf("printed lines\n");
+    /*
+    SDL_Surface *bimage = SDL_CreateRGBSurface(0, 180, 2*rho, 32, 0, 0, 0, 0);
+    int val;
+    Uint32 pixel;
+     printf("after creating image\n");
+    for(int j = 0; j < h; j++)
+    {
+        for(int i = 0; i < w; i++)
+        {
+            val = accumulator[j][i];
+            pixel = SDL_MapRGB(bimage->format, val, val, val);
             put_pixel(bimage, i, j, pixel);
         }
     }
-    SDL_Surface *n_image = display_image(image);
+    SDL_Surface *n_image = display_image(bimage);
     wait_for_keypressed();
     SDL_FreeSurface(n_image);
     SDL_FreeSurface(bimage);
+    */
+    SDL_Surface *biimage = display_image(image);
+    wait_for_keypressed();
+    SDL_FreeSurface(biimage);
 }
