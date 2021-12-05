1abf104784915a01b67f1e4cd7c677fff4f1c956
ana2401 <77988773+ana2401@users.noreply.github.com>
2021-12-05 17:19:35 +0100

    Update user_interface.c

--- UserInterface/user_interface.c
+++ UserInterface/user_interface.c
@@ -14,9 +14,13 @@ typedef struct UserInterface
     GtkWindow *window;
     SDL_Surface *image;
     GtkButton* load;
+    GtkButton* grid;
     GtkButton* prepro;
-    GtkButton* solver;
+    GtkButton* lines;
     GtkButton* result;
+    GtkButton* split;
+    GtkButton* train;
+    GtkButton* test;
     GtkImage *display;
 }UserInterface;
 
@@ -44,6 +48,7 @@ gboolean load_image(GtkWidget *widget, gpointer user_data)
         if(SDL_Init(SDL_INIT_VIDEO) == -1)
             errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
         ui->image = loadImage(filename);
+        g_print("%s\n", filename);
         GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(filename, 600, 400, TRUE, NULL);
         gtk_image_set_from_pixbuf(ui->display,pixbuf);
     }
@@ -60,26 +65,52 @@ gboolean prepro(GtkWidget *widget, gpointer user_data)
     {
         const char *filename = preprocess(ui->image);
         ui->image = loadImage((char*)filename);
+        g_print("%s\n", filename);
         GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(filename, 600, 400, TRUE, NULL);
         gtk_image_set_from_pixbuf(ui->display,pixbuf);
         return TRUE;
     }
 }
 
-gboolean solve_sudoku(GtkWidget *widget, gpointer user_data)
+gboolean detectEdge(GtkWidget *widget, gpointer user_data)
 {
     UserInterface *ui = user_data;
     if(ui->image == NULL)
         return load_image(widget, user_data);
     else
     {
-        //gchar *pathh = "image_05.jpeg";
         const char *filename = edge_detection(ui->image);
         ui->image = loadImage((char*)filename);
+        g_print("%s\n", filename);
         GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(filename, 600, 400, TRUE, NULL);
         gtk_image_set_from_pixbuf(ui->display,pixbuf);
         return TRUE;
     }
+}
+
+gboolean gridDetect(GtkWidget *widget, gpointer user_data)
+{
+    UserInterface *ui = user_data;
+    squaredetect();
+    ui->image = loadImage("final_square.bmp");
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale("final_square.bmp", 600, 400, TRUE, NULL);
+    gtk_image_set_from_pixbuf(ui->display,pixbuf);
+    return TRUE;
+}
+
+gboolean split_func(GtkWidget *widget, gpointer user_data)
+{
+    return TRUE;
+}
+
+gboolean train_func(GtkWidget *widget, gpointer user_data)
+{
+    return TRUE;
+}
+
+gboolean test_func(GtkWidget *widget, gpointer user_data)
+{
+    return TRUE;
 }
 
 gboolean displayy(GtkWidget *widget, gpointer user_data)
@@ -87,6 +118,7 @@ gboolean displayy(GtkWidget *widget, gpointer user_data)
     // display image
     return TRUE;
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -102,16 +134,24 @@ int main(int argc, char *argv[])
     GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "OCR.ui"));
     GtkButton* load = GTK_BUTTON(gtk_builder_get_object(builder, "button_load"));
     GtkButton* pre = GTK_BUTTON(gtk_builder_get_object(builder, "pre_button"));
-    GtkButton* solver = GTK_BUTTON(gtk_builder_get_object(builder, "solver_button"));
+    GtkButton* grid = GTK_BUTTON(gtk_builder_get_object(builder, "grid_button"));
+    GtkButton* lines = GTK_BUTTON(gtk_builder_get_object(builder, "solver_button"));
     GtkButton* result = GTK_BUTTON(gtk_builder_get_object(builder, "result_button"));
+    GtkButton* split = GTK_BUTTON(gtk_builder_get_object(builder, "split_button"));
+    GtkButton* train = GTK_BUTTON(gtk_builder_get_object(builder, "train_button"));
+    GtkButton* test = GTK_BUTTON(gtk_builder_get_object(builder, "test_button"));
     GtkImage* display = GTK_IMAGE(gtk_builder_get_object(builder, "image"));
 
     UserInterface ui =
     {
         .image = NULL,
+        .grid = grid,
+        .split = split,
+        .train = train,
+        .test = test,
         .window = window,
         .load = load,
-        .solver = solver,
+        .lines = lines,
         .result = result,
         .display = display,
     };
@@ -119,7 +159,11 @@ int main(int argc, char *argv[])
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(load, "clicked", G_CALLBACK(load_image), &ui);
     g_signal_connect(pre, "clicked", G_CALLBACK(prepro), &ui);
-    g_signal_connect(solver, "clicked", G_CALLBACK(solve_sudoku), &ui);
+    g_signal_connect(grid, "clicked", G_CALLBACK(gridDetect), &ui);
+    g_signal_connect(split, "clicked", G_CALLBACK(split_func), &ui);
+    g_signal_connect(train, "clicked", G_CALLBACK(train_func), &ui);
+    g_signal_connect(test, "clicked", G_CALLBACK(test_func), &ui);
+    g_signal_connect(lines, "clicked", G_CALLBACK(detectEdge), &ui);
     g_signal_connect(result, "clicked", G_CALLBACK(displayy), NULL);
 
     gtk_main();
